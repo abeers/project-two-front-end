9e82e9bd6605a900fae09753ce6a1bcbf38f24c0
abeers <alexbeers92@gmail.com>
2016-10-05 00:46:50 -0400

    Prepares for deployment

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(57);
+	module.exports = __webpack_require__(59);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 57:
+/***/ 59:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
